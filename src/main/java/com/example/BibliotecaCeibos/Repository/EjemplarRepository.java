49afff3f8d9d100111ae3df1f5a9edbb4221d89e
Gabriel <dascem12mainpaladina@gmail.com>
2025-12-01 01:09:21 -0500

    Fix permisos mvnw

--- src/main/java/com/example/BibliotecaCeibos/Repository/EjemplarRepository.java
+++ src/main/java/com/example/BibliotecaCeibos/Repository/EjemplarRepository.java
@@ -3,12 +3,17 @@ package com.example.BibliotecaCeibos.Repository;
 import com.example.BibliotecaCeibos.Entity.Ejemplar;
 import com.example.BibliotecaCeibos.Entity.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 import java.util.Optional;
 
-public interface EjemplarRepository extends JpaRepository<Ejemplar,Integer>{
-    
+@Repository
+public interface EjemplarRepository extends JpaRepository<Ejemplar, Integer> {
+
+    // ✅ OPTIMIZACIÓN: Trae el Ejemplar Y los datos del Libro en 1 sola consulta
     @Query("SELECT e FROM Ejemplar e LEFT JOIN FETCH e.libro")
     List<Ejemplar> findAllWithDetails();
-    
+
     Optional<Ejemplar> findFirstByLibroAndEstado(Libro libro, String estado);
 }
